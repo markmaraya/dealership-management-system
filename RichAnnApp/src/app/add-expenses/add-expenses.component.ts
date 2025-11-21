9165ba29f8fcdaf4ca78a4e42a2ed1e3c72f35eb
markmaraya <markanthonymaraya@gmail.com>
2025-11-21 13:28:41 +0200

    Initial commit of existing app

---
+++ RichAnnApp/src/app/add-expenses/add-expenses.component.ts
@@ -0,0 +1,76 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import * as io from 'socket.io-client';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from "@angular/material/dialog";
+
+/** Error when invalid control is dirty, touched, or submitted. */
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
+@Component({
+  selector: 'app-add-expenses',
+  templateUrl: './add-expenses.component.html',
+  styleUrls: ['./add-expenses.component.css']
+})
+export class AddExpensesComponent implements OnInit {
+  socket = io('http://localhost:4000');
+
+  salesForm: FormGroup;
+  _id = '';
+  _unitCode = '';
+  amount = '';
+  description = '';
+  encodedBy = '';
+  dateEncoded: Date;
+  isLoadingResults = false;
+  matcher = new MyErrorStateMatcher();
+
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    private formBuilder: FormBuilder,
+    public dialogRef: MatDialogRef<AddExpensesComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+    ) { }
+
+  ngOnInit(): void {
+    this._id = this.data._id;
+    this._unitCode = this.data.unitCode;
+    this.salesForm = this.formBuilder.group({
+      amount: [null, Validators.required],
+      description: [null, Validators.required],
+      encodedBy: [null, Validators.required]
+    });
+  }
+
+  onFormSubmit() {
+    this.isLoadingResults = true;    
+
+    this.salesForm.setValue({
+      amount : this.salesForm.value.amount,
+      description : this.salesForm.value.description.toLowerCase(),
+      encodedBy : this.salesForm.value.encodedBy.toLowerCase()
+    });
+
+    this.salesForm.value.unitCode = this._id;
+    this.salesForm.value.dateEncoded = Date.now();
+    
+    this.api.addExpenses(this.salesForm.value)
+      .subscribe((res: any) => {
+        const id = res._id;
+        this.isLoadingResults = false;
+        this.socket.emit('updatedata', res);
+        this.router.navigate(['/sales-details', this._id]);
+      }, (err: any) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      });
+  }
+}
---
+++ RichAnnApp/src/app/sales-details/sales-details.component.ts
@@ -0,0 +1,228 @@
+import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MatDialog } from "@angular/material/dialog";
+import { Clipboard } from "@angular/cdk/clipboard";
+import { DatePipe } from '@angular/common';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiService } from '../api.service';
+import { Units } from './../units';
+import { Gallery } from '../gallery';
+import { ConfirmationDialogComponent } from '../confirmation-dialog/confirmation-dialog.component';
+import { SalesImageUploadComponent } from '../sales-image-upload/sales-image-upload.component';
+import { ImagePreviewDialogComponent } from '../image-preview-dialog/image-preview-dialog.component';
+import { AddExpensesComponent } from '../add-expenses/add-expenses.component';
+import { EditSalesComponent } from '../edit-sales/edit-sales.component';
+import { EditExpensesComponent } from '../edit-expenses/edit-expenses.component';
+
+
+@Component({
+  selector: 'app-sales-details',
+  templateUrl: './sales-details.component.html',
+  styleUrls: ['./sales-details.component.css']
+})
+export class SalesDetailsComponent implements OnInit {
+  socket = io('http://localhost:4000');
+
+  _id: any;
+  gallery: Gallery = { id: '', imageUrl: '', uploaded: null, unitCode: '' };
+  units: Units = { id: '', unitCode: '', makeAndModel: '', bodyType: '', chasisCode: '', status: '', expenses: [], imageFile: this.gallery };
+  displayedColumns: string[] = ['amount', 'description', 'encodedBy', 'dateEncoded', 'actions'];
+  printDisplayedColumns: string[] = ['amount', 'description', 'encodedBy', 'dateEncoded'];
+  isLoadingResults = true;
+  showExpenses = 0;
+  total = 0;
+  salesForm: FormGroup;
+
+  constructor(
+    private route: ActivatedRoute,
+    private api: ApiService,
+    private router: Router,
+    public dialog: MatDialog,
+    private clipboard: Clipboard,
+    private datePipe: DatePipe,
+    private formBuilder: FormBuilder
+  ) { }
+
+  ngOnInit(): void {
+    this._id = this.route.snapshot.params.id;
+    this.getUnitsDetails(this._id);
+
+    this.socket.on('update-data', function (data: any) {
+      this.getUnitsDetails(this._id);
+    }.bind(this));
+  }
+
+  getUnitsDetails(id: string) {
+    this.api.getUnitsById(id)
+      .subscribe((data: any) => {
+        this.units = data;
+        console.log(this.units);
+        this.getExpenses(id);
+        this.isLoadingResults = false;
+      });
+  }
+
+  deleteUnits(id: any) {
+    this.isLoadingResults = true;
+    this.api.deleteUnits(id)
+      .subscribe(res => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/']);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+      );
+  }
+
+  getExpenses(id: string) {
+    this.api.getExpensesByUnitCode(id)
+      .subscribe((res: any) => {
+        this.units.expenses = res;
+        this.showExpenses = this.units.expenses.length;
+        this.calculateTotal();
+        console.log(this.units.expenses);
+      }, err => {
+        console.log(err);
+        this.isLoadingResults = false;
+        this.showExpenses = 0;
+      });
+  }
+
+  openAddExpensesDialog(id: any, unitCode: string) {
+    this.dialog.open(AddExpensesComponent, {
+      data: {
+        _id: id,
+        unitCode: unitCode
+      }
+    });
+  }
+
+  openUploadDialog(id: any) {
+    const uploadDialog = this.dialog.open(SalesImageUploadComponent, {
+      data: { _id: id }
+    });
+    uploadDialog.afterClosed().subscribe(result => {
+      if (this.route.snapshot.params.imgId) {
+        this.isLoadingResults = true;
+        this.salesForm = this.formBuilder.group({
+          unitCode: [null, Validators.required],
+          makeAndModel: [null, Validators.required],
+          bodyType: [null, Validators.required],
+          chasisCode: [null, Validators.required],
+          expenses: null,
+          imageFile: [null, Validators.required]
+        });
+        this.updateUnitAddImage(id, this.route.snapshot.params.imgId);
+      }
+    });
+  }
+
+  openEditUnitDialog(id: string) {
+    this.dialog.open(EditSalesComponent, {
+      data: { _id: id }
+    });
+  }
+
+  openDeleteUnitDialog(id: any) {
+    const confirmDialog = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        title: 'Confirm Remove Unit',
+        message: `Are you sure, you want to remove unit ${this.units.unitCode}`
+      }
+    });
+    confirmDialog.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteUnits(id);
+      }
+    });
+  }
+
+  openEditExpensesDialog(unitId: string, expenseId: string) {
+    this.dialog.open(EditExpensesComponent, {
+      data: { 
+        unitId: unitId,
+        expensesId: expenseId
+      }
+    });
+  }
+
+  openDialogDeleteExpenses(id: any) {
+    const confirmDialog = this.dialog.open(ConfirmationDialogComponent, {
+      data: {
+        title: 'Confirm Remove Expenses',
+        message: 'Are you sure, you want to remove expenses'
+      }
+    });
+    confirmDialog.afterClosed().subscribe(result => {
+      if (result === true) {
+        this.deleteExpenses(id);
+      }
+    });
+  }
+
+  openImagePreviewDialog(id: any) {
+    this.dialog.open(ImagePreviewDialogComponent, {
+      data: { _id: id }
+    });
+  }
+
+  deleteExpenses(id: any) {
+    this.isLoadingResults = true;
+    this.api.deleteExpenses(id)
+      .subscribe(res => {
+        this.isLoadingResults = false;
+        this.router.navigate(['/sales-details', this._id]);
+        this.socket.emit('updatedata', res);
+      }, (err) => {
+        console.log(err);
+        this.isLoadingResults = false;
+      }
+      );
+  }  
+
+  copyToClipboard(amount: string, description: string, encodedBy: string, dateEncoded: string) {
+    this.clipboard.copy(`${amount.toUpperCase()} ${description.toUpperCase()} ${encodedBy.toUpperCase()} ${this.datePipe.transform(dateEncoded, 'MMM d, y, h:mm:ss a')}`);
+  }
+
+  updateUnitAddImage(id: string, imgId: string): void {
+    this.api.getGalleryById(imgId)
+      .subscribe((data: any) => {
+        this.isLoadingResults = false;
+        this.checkUnitCode(id, data);
+      });
+  }
+
+  private calculateTotal() {
+    var amounts = this.units.expenses.map(x => { return Number(x.amount) }
+    );
+    this.total = amounts.reduce((accum, curr) => accum + curr, 0);
+  }
+
+  private checkUnitCode(id: string, gallery: Gallery) {
+    this.api.getUnitsById(id).subscribe((result: any) => {
+      this.isLoadingResults = false;
+      this.salesForm.setValue({
+        unitCode: result.unitCode,
+        makeAndModel: result.makeAndModel,
+        bodyType: result.bodyType,
+        chasisCode: result.chasisCode,
+        expenses: result.expenses,
+        imageFile: gallery
+      });
+
+      this.api.updateUnits(id, this.salesForm.value)
+        .subscribe((res: any) => {
+          this.isLoadingResults = false;
+          this.socket.emit('updatedata', res);
+          this.router.navigate(['/sales-details', id]);
+        }, (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+        );
+    });
+  }
+
+}
