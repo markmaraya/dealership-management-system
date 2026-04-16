5c7310788a2800d6ca8fd5d2265cb17432423ae4
Mark Maraya <markanthonymaraya@gmail.com>
2026-04-16 18:13:55 +0800

    style: improve available-units layout and visuals

--- frontend/src/app/units/available-units/available-units.component.ts
+++ frontend/src/app/units/available-units/available-units.component.ts
@@ -1,9 +1,16 @@
 import { environment } from '../../../environments/environment';
 import { Component, OnInit, ViewChild } from '@angular/core';
-import { UntypedFormControl, FormGroupDirective, UntypedFormBuilder, UntypedFormGroup, NgForm, Validators } from '@angular/forms';
+import {
+  UntypedFormControl,
+  FormGroupDirective,
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  NgForm,
+  Validators,
+} from '@angular/forms';
 import { UpperCasePipe, Location } from '@angular/common';
 import { Router } from '@angular/router';
-import { io } from "socket.io-client";
+import { io } from 'socket.io-client';
 import { ApiService } from '../../api.service';
 import { Units } from '../../units';
 import { MatPaginator } from '@angular/material/paginator';
@@ -16,9 +23,16 @@ import { AddSalesComponent } from '../../add-sales/add-sales.component';
 
 /** Error when invalid control is dirty, touched, or submitted. */
 export class MyErrorStateMatcher implements ErrorStateMatcher {
-  isErrorState(control: UntypedFormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+  isErrorState(
+    control: UntypedFormControl | null,
+    form: FormGroupDirective | NgForm | null,
+  ): boolean {
     const isSubmitted = form && form.submitted;
-    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+    return !!(
+      control &&
+      control.invalid &&
+      (control.dirty || control.touched || isSubmitted)
+    );
   }
 }
 
@@ -26,7 +40,7 @@ export class MyErrorStateMatcher implements ErrorStateMatcher {
   selector: 'app-available-units',
   templateUrl: './available-units.component.html',
   styleUrls: ['./available-units.component.css'],
-  providers: [UpperCasePipe]
+  providers: [UpperCasePipe],
 })
 export class AvailableUnitsComponent implements OnInit {
   socket = io(environment.apiUrl);
@@ -37,9 +51,16 @@ export class AvailableUnitsComponent implements OnInit {
   salesForm: UntypedFormGroup;
   unitsForm: UntypedFormGroup;
   matcher = new MyErrorStateMatcher();
-  displayedColumns: string[] = ['unitCode', 'image', 'makeAndModel', 'bodyType', 'chasisCode', 'actions'];
+  displayedColumns: string[] = [
+    'unitCode',
+    'image',
+    'makeAndModel',
+    'bodyType',
+    'chasisCode',
+    'actions',
+  ];
   data: any;
-  isLoadingResults: boolean = true;  
+  isLoadingResults: boolean = true;
   searchWord: any;
   searchBy: any;
   formOptions: UntypedFormGroup;
@@ -47,9 +68,7 @@ export class AvailableUnitsComponent implements OnInit {
   floatLabelControl = new UntypedFormControl('auto');
   tableState: any = {
     isSold: true,
-    isTableColumnHidden: false,    
-    tableToogleButtonText: 'Sold Units',
-    tableLabelStatus: 'Available Units'
+    isTableColumnHidden: false,
   };
   environmentApiUrl = environment.apiUrl;
 
@@ -58,47 +77,47 @@ export class AvailableUnitsComponent implements OnInit {
     private fb: UntypedFormBuilder,
     private router: Router,
     public dialog: MatDialog,
-    private location:Location,
-    private uppercasePipe: UpperCasePipe) {
+    private location: Location,
+    private uppercasePipe: UpperCasePipe,
+  ) {
     this.formOptions = fb.group({
       hideRequired: this.hideRequiredControl,
       floatLabel: this.floatLabelControl,
-    });    
+    });
   }
 
   ngOnInit(): void {
     let status = history.state.status == 'sold' ? true : false;
     this.getUnits(status);
 
-    this.socket.on('update-data', function (data: any) {
-      this.getUnits();
-    }.bind(this));
+    this.socket.on(
+      'update-data',
+      function (data: any) {
+        this.getUnits();
+      }.bind(this),
+    );
 
     this.initForm();
   }
 
-  getUnits(isSold?: boolean) {  
-    this.api.getUnits()
-      .subscribe((res: any) => {      
+  getUnits(isSold?: boolean) {
+    this.api.getUnits().subscribe(
+      (res: any) => {
         let getFilteredUnits = (res) => {
           if (isSold) {
             this.tableState = {
               isSold: false,
-              isTableColumnHidden: true,              
-              tableToogleButtonText: 'Available Units',
-              tableLabelStatus: 'Sold Units'
+              isTableColumnHidden: true,
             };
             return res.status == 'sold';
           } else {
             this.tableState = {
               isSold: true,
-              isTableColumnHidden: false,              
-              tableToogleButtonText: 'Sold Units',
-              tableLabelStatus: 'Available Units'
+              isTableColumnHidden: false,
             };
             return res.status != 'sold';
           }
-        }
+        };
 
         let filteredRes: Units[] = res.filter(getFilteredUnits);
 
@@ -106,20 +125,25 @@ export class AvailableUnitsComponent implements OnInit {
         this.data.paginator = this.paginator;
         this.data.sort = this.sort;
         if (this.salesForm.value.searchWord) {
-          this.applyFilter(this.salesForm.value.searchWord, this.salesForm.value.searchBy);
+          this.applyFilter(
+            this.salesForm.value.searchWord,
+            this.salesForm.value.searchBy,
+          );
         }
         console.log(this.data);
         this.isLoadingResults = false;
-      }, err => {
+      },
+      (err) => {
         console.log(err);
         this.isLoadingResults = false;
-      });
+      },
+    );
   }
 
   initForm() {
     this.salesForm = this.fb.group({
       searchWord: [null, Validators.required],
-      searchBy: null
+      searchBy: null,
     });
   }
 
@@ -151,20 +175,23 @@ export class AvailableUnitsComponent implements OnInit {
           }
           break;
       }
-    }
+    };
     filterValue = filterValue.trim();
     filterValue = filterValue.toLowerCase();
     this.data.filter = filterValue;
   }
 
   onFormSubmit() {
-    this.applyFilter(this.salesForm.value.searchWord, this.salesForm.value.searchBy);
+    this.applyFilter(
+      this.salesForm.value.searchWord,
+      this.salesForm.value.searchBy,
+    );
   }
 
   clearSearch() {
     this.salesForm = new UntypedFormGroup({
       searchWord: new UntypedFormControl(''),
-      searchBy: new UntypedFormControl(0)
+      searchBy: new UntypedFormControl(0),
     });
     this.salesForm.reset();
     this.getUnits(!this.tableState.isSold);
@@ -174,10 +201,10 @@ export class AvailableUnitsComponent implements OnInit {
     const confirmDialog = this.dialog.open(ConfirmationDialogComponent, {
       data: {
         title: 'Mark Unit as Sold',
-        message: `Confirm unit "${this.uppercasePipe.transform(unit)}" as sold`
-      }
+        message: `Confirm unit "${this.uppercasePipe.transform(unit)}" as sold`,
+      },
     });
-    confirmDialog.afterClosed().subscribe(result => {
+    confirmDialog.afterClosed().subscribe((result) => {
       if (result === true) {
         this.setStatusToSold(id);
       }
@@ -199,19 +226,20 @@ export class AvailableUnitsComponent implements OnInit {
         chasisCode: result.chasisCode,
         status: status,
         expenses: result.expenses,
-        imageFile: result.imageFile
+        imageFile: result.imageFile,
       });
-      
-      this.api.updateUnits(id, this.unitsForm.value)
-          .subscribe((res: any) => {
-            this.isLoadingResults = false;
-            this.socket.emit('updatedata', res);
-            this.router.navigate(['/']);
-          }, (err: any) => {
-            console.log(err);
-            this.isLoadingResults = false;
-          }
-          );
+
+      this.api.updateUnits(id, this.unitsForm.value).subscribe(
+        (res: any) => {
+          this.isLoadingResults = false;
+          this.socket.emit('updatedata', res);
+          this.router.navigate(['/']);
+        },
+        (err: any) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        },
+      );
     });
   }
 }
