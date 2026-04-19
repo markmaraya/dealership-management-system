9bebccdfa33352fb52639ea73e357194d3737f7b
Mark Maraya <markanthonymaraya@gmail.com>
2026-04-19 22:52:57 +0800

    refactor: update component template and adjust property handling

--- frontend/src/app/confirmation-dialog/confirmation-dialog.component.ts
+++ frontend/src/app/confirmation-dialog/confirmation-dialog.component.ts
@@ -4,17 +4,19 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 @Component({
   selector: 'app-confirmation-dialog',
   templateUrl: './confirmation-dialog.component.html',
-  styleUrls: ['./confirmation-dialog.component.css']
+  styleUrls: ['./confirmation-dialog.component.scss'],
 })
 export class ConfirmationDialogComponent implements OnInit {
-  title: string;
-  message: string;
+  title!: string;
+  message!: string;
 
-  constructor(public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: any) { }
-
+  constructor(
+    public dialogRef: MatDialogRef<ConfirmationDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+  ) {}
 
   ngOnInit() {
+    this.title = this.data.title;
+    this.message = this.data.message;
   }
-
 }
